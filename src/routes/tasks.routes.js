ef017d9f13f9694d0eb76e44a3377de779b08be6
JoseBernabe123 <00038121@uca.edu.sv>
2023-12-04 19:38:55 -0600

    allready

---
+++ src/routes/tasks.routes.js
@@ -0,0 +1,19 @@
+import { Router } from "express"
+import { authRequired } from "../middlewares/validateToken.js"
+import { getProducts, getProduct, createProduct, updateProduct, deleteProduct } from '../controllers/tasks.controller.js'
+import { validateSchema } from "../middlewares/validator.middleware.js"
+import { createProductSchema } from "../schemas/product.schema.js"
+
+const router = Router()
+
+router.get('/products', authRequired, getProducts)
+
+router.get('/product/:id', authRequired, getProduct)
+
+router.post('/product', authRequired, validateSchema(createProductSchema), createProduct)
+
+router.delete('/product/:id', authRequired, deleteProduct)
+
+router.put('/product/:id', authRequired, updateProduct)
+
+export default router;
